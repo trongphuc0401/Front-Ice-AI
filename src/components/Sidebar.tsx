036f603013aab1eb60634075b19b643702494631
Nguyen Trong Phuc <93772238+trongphuc0401@users.noreply.github.com>
2025-04-01 22:54:33 +0700

    update

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useTheme } from "../context/ThemeContext";
 import "../styles/Sidebar.css";
 
